f0a965ccfede335906c88085a93e1859a3195416
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-04-22 12:03:51 +0200

    Wrote 3 unit tests using mocha for bookingCost function

--- bookings_cost/test/bookings.tests.js
+++ bookings_cost/test/bookings.tests.js
@@ -1,1 +1,28 @@
+describe("Tests for the bookingCost function", () => {
+    it("should cost R135.00 if user has booked for 3 days without discount", () => {
+        const data = {
+            check_in_day: 'Monday',
+            duration: 3,
+            cat_name : 'Snowy'
+        }
+        assert.strictEqual(135.00, bookingCost(data));
+    });
 
+    it("should cost R101.25 if user has booked for 3 days with 25% discount on Wednesday", () => {
+        const data = {
+            check_in_day: 'Wednesday',
+            duration: 3,
+            cat_name : 'Snowy'
+        }
+        assert.strictEqual(101.25, bookingCost(data));
+    });
+
+    it("should cost R114.75 if user has booked for 3 days with 15% discount on Friday", () => {
+        const data = {
+            check_in_day: 'Friday',
+            duration: 3,
+            cat_name : 'Snowy'
+        }
+        assert.strictEqual(114.75, bookingCost(data));
+    });
+});
